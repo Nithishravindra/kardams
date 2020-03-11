befd5a4cdb2de3be18d489c8954f05402ae744d5
nithish ravindra <nithishravindra8@gmail.com>
2020-03-11 17:19:33 +0530

    added README.md/made changes in scrape.js

---
+++ README.md
@@ -0,0 +1,17 @@
+## kardams
+
+Outputs the current waterlevel of major dams of karnataka.
+
+## Install
+```
+npm i kardams -g
+```
+ ## Usuage 
+
+```
+➜  ~  kardams
+```
+
+ ## License
+
+[MIT](LICENSE).
--- scrape.js
+++ scrape.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 "use strict"
 const request = require('request');
 const cheerio = require('cheerio');
@@ -12,32 +13,31 @@ request('https://www.tnagrisnet.tn.gov.in/ARS/home/reservoir', (response, error,
 
     let result = {
         Title: heading.html(),
-        Date: x[1] + "/" + x[2] + "/" + x[3],
+        Date: (x[1] + "/" + x[2] + "/" + x[3]).trim(),
         Dams: [
             {
-                Reservoir: $('body > div.container > div > div > table > tbody > tr:nth-child(3) > td:nth-child(1)').text(),
+                Reservoir: $('body > div.container > div > div > table > tbody > tr:nth-child(3) > td:nth-child(1)').text().trim(),
                 FullDepth: $('body > div.container > div > div > table > tbody > tr:nth-child(3) > td:nth-child(2)').text(),
                 CurrentWaterLevel: $('body > div.container > div > div > table > tbody > tr:nth-child(3) > td:nth-child(4)').text(),
                 LastYearLevel: $('body > div.container > div > div > table > tbody > tr:nth-child(3) > td:nth-child(8)').text()
             },
             {
-                Reservoir: $('body > div.container > div > div > table > tbody > tr:nth-child(9) > td:nth-child(1)').text(),
+                Reservoir: $('body > div.container > div > div > table > tbody > tr:nth-child(9) > td:nth-child(1)').text().trim(),
                 FullDepth: $('body > div.container > div > div > table > tbody > tr:nth-child(9) > td:nth-child(2)').text(),
                 CurrentWaterLevel: $('body > div.container > div > div > table > tbody > tr:nth-child(9) > td:nth-child(4)').text(),
                 LastYearLevel: $('body > div.container > div > div > table > tbody > tr:nth-child(9) > td:nth-child(8)').text()
             },
             {
-                Reservoir: $('body > div.container > div > div > table > tbody > tr:nth-child(5) > td:nth-child(1)').text(),
+                Reservoir: $('body > div.container > div > div > table > tbody > tr:nth-child(5) > td:nth-child(1)').text().trim(),
                 FullDepth: $('body > div.container > div > div > table > tbody > tr:nth-child(5) > td:nth-child(2)').text(),
-                CurrentWaterLevel: $('body > div.container > div > div > table > tbody > tr:nth-child(9) > td:nth-child(4)').text(),
+                CurrentWaterLevel: $('body > div.container > div > div > table > tbody > tr:nth-child(5) > td:nth-child(4)').text(),
                 LastYearLevel: $('body > div.container > div > div > table > tbody > tr:nth-child(5) > td:nth-child(8)').text()
             },
             {
-                Reservoir: $('body > div.container > div > div > table > tbody > tr:nth-child(7) > td:nth-child(1)').text(),
+                Reservoir: $('body > div.container > div > div > table > tbody > tr:nth-child(7) > td:nth-child(1)').text().trim(),
                 FullDepth: $('body > div.container > div > div > table > tbody > tr:nth-child(7) > td:nth-child(2)').text(),
                 CurrentWaterLevel: $('body > div.container > div > div > table > tbody > tr:nth-child(7) > td:nth-child(4)').text(),
                 LastYearLevel: $('body > div.container > div > div > table > tbody > tr:nth-child(7) > td:nth-child(8)').text()
-
             }
         ]
     }
